4e266b4f4ae71dc3b8cbd7299157fba32a4a2fd8
wwwolfsasa <wwwolfsasa@gmail.com>
2021-01-09 11:01:55 +0800

    加入 TableName Attribute

--- SASA.SAMS.HMI.WEB/Models/DatabaseAttribute.cs
+++ SASA.SAMS.HMI.WEB/Models/DatabaseAttribute.cs
@@ -5,8 +5,19 @@ using System.Web;
 
 namespace SASA.SAMS.HMI.WEB.Models {
     public class DatabaseAttribute :Attribute {
-
+        /// <summary>
+        /// 資料表
+        /// </summary>
+        public string TableName { get; set; }
+        /// <summary>
+        /// 欄位名稱
+        /// </summary>
         public string ColumnName { get; set; }
+
+        public DatabaseAttribute(string TableName, string ColumnName) {
+            this.TableName = TableName;
+            this.ColumnName = ColumnName;
+        }
 
         public DatabaseAttribute(string ColumnName) {
             this.ColumnName = ColumnName;
